0e5e5192a113b214197d67312cbe712389e03c5d
yosrimhamdi <bavary1515@gmail.com>
2021-05-14 02:24:01 +0100

    connencted a hosted database.

---
+++ makefile
@@ -0,0 +1,16 @@
+
+git:
+		git add .
+		git commit -m "$m"
+		git push origin master
+
+heroku:
+		git push heroku master
+		heroku open
+
+logs:
+		heroku logs --tail
+
+both:
+	$(MAKE) git
+	$(MAKE) heroku
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ import app from './app';
 import mongoose from 'mongoose';
 import 'colors';
 
-mongoose.connect('mongodb://localhost/test', {
-  useNewUrlParser: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connected to database'.green.italic))
+  .catch(console.error);
 
 app.listen(3000, () => {
   console.clear();
-  console.log('server running on localhost:3000'.green);
+  console.log('server running on localhost:3000'.green.italic);
 });

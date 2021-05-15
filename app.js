fe569fde4f435eda94664e0bc8180edbc7c772ae
yosrimhamdi <bavary1515@gmail.com>
2021-05-15 02:19:04 +0100

    changed a moddleware name.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import express from 'express';
 import morgan from 'morgan';
 
 import paymentRouter from './routers/payment';
-import setDomain from './middlewares/setDomain';
+import domain from './middlewares/domain';
 
 const app = express();
 
 app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
-app.use(setDomain);
+app.use(domain);
 app.use(express.raw({ type: 'application/json' }));
 app.use(express.static('./public'));
 
